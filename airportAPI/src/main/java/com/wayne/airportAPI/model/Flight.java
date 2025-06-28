169950b9403abe2df5f5a29d6c3bf418abfa3a1c
WayneJN <waynejnorman@gmail.com>
2025-06-28 00:49:00 -0230

    Update Flight.java

--- airportAPI/src/main/java/com/wayne/airportAPI/model/Flight.java
+++ airportAPI/src/main/java/com/wayne/airportAPI/model/Flight.java
@@ -1,4 +1,41 @@
 package com.wayne.airportAPI.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
 public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+
+    @ManyToOne
+    @JoinColumn(name = "aircraft_id")
+    private Aircraft aircraft;
+
+    @ManyToOne
+    @JoinColumn(name = "origin_airport_id")
+    private Airport originAirport;
+
+    @ManyToOne
+    @JoinColumn(name = "destination_airport_id")
+    private Airport destinationAirport;
+
+    @ManyToMany
+    @JoinTable(
+        name = "flight_passenger",
+        joinColumns = @JoinColumn(name = "flight_id"),
+        inverseJoinColumns = @JoinColumn(name = "passenger_id")
+    )
+    private Set<Passenger> passengers;
 }
